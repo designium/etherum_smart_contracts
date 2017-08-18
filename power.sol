70ad9b6de81d04c2b6bb00533070955b4af21658
Chim Kan <designium@gmail.com>
2017-08-18 16:52:17 -0400

    fixing mistakes.

--- power.sol
+++ power.sol
@@ -4,11 +4,11 @@ contract power {
   uint value;
 
   /* this function is executed at initializatin of the contract */
-  function power(unit number, uint p) {
+  function power(uint number, uint p) {
     value = number ** p;
   }
 
-  function getPower() constant returns (unit) {
+  function getPower() constant returns (uint) {
     return value;
   }
 }
