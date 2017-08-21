9f2e0df164f7aac567295acad434bc84cf655c4e
Chim Kan <designium@gmail.com>
2017-08-21 11:20:36 -0400

    Lottery example

---
+++ lottery.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.11;
+
+mapping(address => uint) usersBet;
+mapping(uint => address) users;
+uint nbUsers = 0;
+uint totalBets = 0;
+
+address owner;
+
+function Bet() public payable {
+  if (msg.value > 0) {
+    if (usersBet[msg.sender] == 0) { // Is it a new player
+        users[nbUsers] = msg.sender;
+        nbUsers += 1;
+    }
+    usersBet[msg.sender] += msg.value;
+    totalBets += msg.value;
+  }
+}
+
+function EndLottery() public {
+  if (msg.sender == owner) {
+      uint sum = 0;
+      uint winningNumber = uint(block.blockhash(block.number -1))% totalBets;
+      for (uint i=0; i < nbUsers; i++) {
+          sum += usersBet[users[i]];
+          if (sum >= winningNumber) {
+            selfdestruct(users[i]);
+            return;
+          }
+      }
+  }
+}
