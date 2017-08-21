941d18903e7aaed830f147363478c1fbf1fffee6
Chim Kan <designium@gmail.com>
2017-08-21 11:49:53 -0400

    updated lottery with modifier..

--- lottery.sol
+++ lottery.sol
@@ -1,33 +1,44 @@
 pragma solidity ^0.4.11;
 
-mapping(address => uint) usersBet;
-mapping(uint => address) users;
-uint nbUsers = 0;
-uint totalBets = 0;
+contract Lottery {
 
-address owner;
+    mapping(address => uint) usersBet;
+    mapping(uint => address) users;
+    uint nbUsers = 0;
+    uint totalBets = 0;
 
-function Bet() public payable {
-  if (msg.value > 0) {
-    if (usersBet[msg.sender] == 0) { // Is it a new player
-        users[nbUsers] = msg.sender;
-        nbUsers += 1;
+    address owner;
+
+    modifier isAdmin() {
+      require(msg.sender == owner);
+      _;
     }
-    usersBet[msg.sender] += msg.value;
-    totalBets += msg.value;
-  }
-}
 
-function EndLottery() public {
-  if (msg.sender == owner) {
-      uint sum = 0;
-      uint winningNumber = uint(block.blockhash(block.number -1))% totalBets;
-      for (uint i=0; i < nbUsers; i++) {
-          sum += usersBet[users[i]];
-          if (sum >= winningNumber) {
-            selfdestruct(users[i]);
-            return;
-          }
+    function Lottery() {
+      owner = msg.sender;
+    }
+
+    function Bet() public payable {
+      if (msg.value > 0) {
+        if (usersBet[msg.sender] == 0) { // Is it a new player
+            users[nbUsers] = msg.sender;
+            nbUsers += 1;
+        }
+        usersBet[msg.sender] += msg.value;
+        totalBets += msg.value;
       }
-  }
+    }
+
+    function EndLottery() public isAdmin {
+          uint sum = 0;
+          uint winningNumber = uint(block.blockhash(block.number -1))% totalBets;
+          for (uint i=0; i < nbUsers; i++) {
+              sum += usersBet[users[i]];
+              if (sum >= winningNumber) {
+                selfdestruct(users[i]);
+                return;
+              }
+      }
+    }
+
 }
