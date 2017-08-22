22b4240fbe7b6a936e1bb88e6e9243657708b10e
Chim Kan <designium@gmail.com>
2017-08-22 17:21:39 -0400

    basic coin contract.

---
+++ ideas/app/contracts/simple_coin.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0;
+
+contract Coin {
+  address public minter;
+  mapping (address => uint) public balances;
+
+  event Sent(address from, address to, uint amount);
+
+  function Coin() {
+    minter = msg.sender;
+  }
+
+  function mint(address receiver, uint amount) {
+    if (msg.sender != minter) return;
+    balances[receiver] += amount;
+  }
+
+  function minterOwner() constant returns (address) {
+    return minter;
+  }
+
+  function send(address receiver, uint amount) {
+    if (balances[msg.sender] < amount) return;
+    balances[msg.sender] -= amount;
+    balances[receiver] += amount;
+    Sent(msg.sender, receiver, amount);
+  }
+}
