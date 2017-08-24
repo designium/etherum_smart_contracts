863dd7b64466e8911a6e3d57147947494a779c96
Chim Kan <designium@gmail.com>
2017-08-23 20:06:07 -0400

    more code for the vote.

--- ideas/app/contracts/voting.sol
+++ ideas/app/contracts/voting.sol
@@ -1,2 +1,66 @@
 pragma solidity ^0.4.11;
 
+//https://solidity.readthedocs.io/en/develop/solidity-by-example.html#voting
+
+/// @title Voting with delegation
+
+contract Ballot {
+
+  struct Voter {
+    uint weight;
+    bool voted;
+    address delegate;
+    uint vote;
+  }
+
+  struct Proposal {
+    bytes32 name;
+    uint voteCount;
+  }
+
+  address public chairperson;
+
+  mapping(address => Voter) public voters;
+
+  Proposal[] public proposals;
+
+  function Ballot(bytes32[] proposalNames) {
+    chairperson = msg.sender;
+    voters[chairperson].weight = 1;
+
+    for (uint i=0; i < proposalNames.length; i++) {
+      proposals.push(Proposal({name: proposalNames[i], voteCount: 0
+        }));
+    }
+  }
+
+  function giveRightToVote(address voter) {
+    require((msg.sender == chairperson) && !voters[voter].voted && (voters[voter].weight == 0));
+    voters[voter].weight = 1;
+  }
+
+  function delegate(address to) {
+    Voter storage sender = voters[msg.sender];
+    require(!sender.voted);
+
+    require(to != msg.sender);
+
+    while (voters[to].delegate != address(0)) {
+      to = voters[to].delegate;
+
+      require(to != msg.sender);
+    }
+
+    sender.voted = true;
+    sender.delegate = to;
+    Voter delegate = voters[to];
+    if (delegate.voted) {
+      proposals[delegate.vote].voteCount += sender.weight;
+      } else {
+        delegate.weight += sender.weight;
+      }
+  }
+
+
+
+}
