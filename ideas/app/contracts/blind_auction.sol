fe1fbac4d8377adef4bba61af093f46914424500
Chim Kan <designium@gmail.com>
2017-08-24 12:00:01 -0400

    made some fixes on my copied code and then fix a warning for the example.

--- ideas/app/contracts/blind_auction.sol
+++ ideas/app/contracts/blind_auction.sol
@@ -8,7 +8,7 @@ contract BlindAuction {
 
   address public beneficiary;
   uint public auctionStart;
-  uint public bliddingEnd;
+  uint public biddingEnd;
   uint public revealEnd;
   bool public ended;
 
@@ -21,7 +21,77 @@ contract BlindAuction {
 
   event AuctionEnded(address winner, uint highestBid);
 
+  modifier onlyBefore(uint _time) { require(now < _time); _; }
+  modifier onlyAfter(uint _time) { require(now > _time); _; }
 
+  function BlindAuction(
+    uint _biddingTime,
+    uint _revealTime,
+    address _beneficiary
+    ) {
+    beneficiary = _beneficiary;
+    auctionStart = now;
+    biddingEnd = now + _biddingTime;
+    revealEnd = biddingEnd + _revealTime;
+  }
+
+  function bid(bytes32 _blindedBid)
+    payable
+    onlyBefore(biddingEnd) {
+    bids[msg.sender].push(Bid({
+      blindedBid: _blindedBid,
+      deposit: msg.value
+      }));
+  }
+
+  function reveal( uint[] _values, bool[] _fake, bytes32[] _secret) onlyAfter(biddingEnd) onlyBefore(revealEnd) {
+    uint length = bids[msg.sender].length;
+    require(_values.length == length);
+    require(_fake.length == length);
+    require(_secret.length == length);
+
+    uint refund;
+
+    for (uint i = 0; i < length; i++) {
+      var bid = bids[msg.sender][i];
+      var (value, fake,secret) = (_values[i], _fake[i], _secret[i]);
+      if (bid.blindedBid != keccak256(value, fake, secret)) {
+        continue;
+      }
+      refund += bid.deposit;
+      if (!fake && bid.deposit >= value) {
+        if (placeBid(msg.sender, value))
+          refund -= value;
+      }
+      bid.blindedBid = bytes32(0);
+    }
+    msg.sender.transfer(refund);
+  }
+
+  function placeBid(address bidder, uint value) internal returns (bool success) {
+    if (value <= highestBid) {
+      pendingReturns[highestBidder] += highestBid;
+    }
+    highestBid = value;
+    highestBidder = bidder;
+    return true;
+  }
+
+  function withdraw() {
+    uint amount = pendingReturns[msg.sender];
+    if (amount > 0) {
+      pendingReturns[msg.sender] = 0;
+
+      msg.sender.transfer(amount);
+    }
+  }
+
+  function auctionEnd() onlyAfter(revealEnd) {
+    require(!ended);
+    AuctionEnded(highestBidder, highestBid);
+    ended = true;
+    beneficiary.transfer(this.balance);
+  }
 
   // end of the contract
 }
