8bc352c20f1086308ec6c2342addfbaaf9047f7c
Chim Kan <designium@gmail.com>
2017-08-25 20:43:11 -0400

    adding funciton to adopt. bad if statemetn.

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -1,5 +1,15 @@
 pragma solidity ^0.4.4;
 
 contract Adoption {
+  address[16] public adopters;
 
+  function adopt(unit petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
+
+    adopters[petId] = msg.sender;
+
+    return petId;
+  }
+
+  // end of the contract
 }
