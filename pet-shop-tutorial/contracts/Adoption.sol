ac3c0bc45354d50dd5a5439d27adffe441824500
Chim Kan <designium@gmail.com>
2017-08-26 21:13:53 -0400

    fixed a typo and added the getadopters funciton.

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -3,13 +3,19 @@ pragma solidity ^0.4.4;
 contract Adoption {
   address[16] public adopters;
 
-  function adopt(unit petId) public returns (uint) {
+  function adopt(uint petId) public returns (uint) {
     require(petId >= 0 && petId <= 15);
 
     adopters[petId] = msg.sender;
 
     return petId;
   }
+
+  function getAdopters() public returns (address[16]) {
+    return adopters;
+  }
+
+
 
   // end of the contract
 }
