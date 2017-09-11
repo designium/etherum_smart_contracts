9d49f1be609c81c982ebad742c2ceae7b0c32092
Chim Kan <designium@gmail.com>
2017-09-11 15:19:01 -0400

    added modifiers.

--- safe_remote_purchase.sol
+++ safe_remote_purchase.sol
@@ -7,6 +7,32 @@ contract Purchase {
   enum State { Created, Locked, Inactive }
   State public state;
 
+  function Purchase() payable {
+    seller = msg.sender;
+    value = msg.value / 2;
+    require((2 * value) == msg.value);
+  }
+
+  modifier condition(bool _condition) {
+    require(_condition);
+    _;
+  }
+
+  modifier onlyBuyer() {
+    require(msg.sender == buyer);
+    _;
+  }
+
+  modifier onlySeller() {
+    require(msg.sender == seller);
+    _;
+  }
+
+  modifier inState(State _state) {
+    require(state == _state);
+    _;
+  }
+
 
 
   // end of contract
