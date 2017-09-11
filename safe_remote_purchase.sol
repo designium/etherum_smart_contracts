c42a455386438ffd7c585ce84c4f8a7a96886311
Chim Kan <designium@gmail.com>
2017-09-11 15:29:17 -0400

    added more functions.

--- safe_remote_purchase.sol
+++ safe_remote_purchase.sol
@@ -33,7 +33,38 @@ contract Purchase {
     _;
   }
 
+  event Aborted();
+  event PurchaseConfirmed();
+  event ItemReceived();
 
+  function abort()
+    onlySeller
+    inState(State.Created)
+    {
+      Aborted();
+      state = State.Inactive;
+      seller.transfer(this.balance);
+    }
 
+    function confirmPurchase()
+      inState(State.Created)
+      condition(msg.value == (2 * value))
+      payable
+      {
+        PurchaseConfirmed();
+        buyer = msg.sender;
+        state = State.Locked;
+      }
+
+      function confirmReceived()
+        onlyBuyer
+        inState(State.Locked)
+        {
+          ItemReceived();
+          state = State.Inactive;
+
+          buyer.transfer(value);
+          seller.transfer(this.balance);
+        }
   // end of contract
 }
